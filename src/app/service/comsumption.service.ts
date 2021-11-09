2434be4e4d86ce170ee2d6ee83858d01978d7124
Ali Marzouk <marzoukali64@gmail.com>
2021-11-09 18:14:28 +0100

    change env

--- src/app/service/comsumption.service.ts
+++ src/app/service/comsumption.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {map} from "rxjs";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: "root",
@@ -10,7 +11,7 @@ export class ComsumptionService {
   }
 
   getComsumptionByHours(n: any) {
-    return this.http.get('http://localhost:5000/electricity_consumption', {
+    return this.http.get(environment.API_BASE +'/electricity_consumption', {
       params: {
         n: n,
       }
